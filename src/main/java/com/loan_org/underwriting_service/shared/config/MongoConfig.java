94cc16412dc20ba2687aaeb2f4aa52354ff5cbe3
Aman Raj <billygates124356@gmail.com>
2026-06-22 15:50:25 +0530

    Updated MongoConfig to use auto index creation

--- src/main/java/com/loan_org/underwriting_service/shared/config/MongoConfig.java
+++ src/main/java/com/loan_org/underwriting_service/shared/config/MongoConfig.java
@@ -1,10 +1,35 @@
 package com.loan_org.underwriting_service.shared.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
+/**
+ * Defines the MongoDB configuration associated with the Underwriting 
+ * Application. 
+ * 
+ * Enables Mongo auditing to allow for the dynamic update of creating and updated
+ * timestamps.
+ * 
+ * Enables timestamps by using auto-index-creation = true as part of the bean creation
+ * for mongo mapper context.
+ * 
+ * @author amanfoundongithub
+ * @version 1.0.0
+ * 
+ * @category Configuration
+ * 
+ */
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
+
+    @Bean
+    public MongoMappingContext mappingContext() {
+        MongoMappingContext mappingContext = new MongoMappingContext();
+        mappingContext.setAutoIndexCreation(true);
+        return mappingContext;
+    }
     
 }
