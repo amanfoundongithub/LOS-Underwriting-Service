342fa69612df2ef737f611cd8b49cd924f004af4
Aman Raj <billygates124356@gmail.com>
2026-06-22 15:53:13 +0530

    added a parent custom underwriting exception

---
+++ src/main/java/com/loan_org/underwriting_service/shared/exception/UnderwritingServiceException.java
@@ -0,0 +1,21 @@
+package com.loan_org.underwriting_service.shared.exception;
+
+/**
+ * Defined as the generic exception that handles all the underwriting 
+ * related exceptions as they occur during the processing of the loan.
+ * 
+ * This is the parent custom exception so that all exceptions can be grouped
+ * under this exception itself.
+ * 
+ * @author amanfoundongithub
+ * @version 1.0.0
+ * 
+ * @category Exception
+ */
+public class UnderwritingServiceException extends RuntimeException {
+    
+    public UnderwritingServiceException(RuntimeException e) {
+        super(e);
+    }
+
+}
